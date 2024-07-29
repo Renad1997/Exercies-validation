fd410a1bf05b36b52fa7bc5d6e6bceae40b1b5a2
Renad Almana <52034735+Renad1997@users.noreply.github.com>
2024-07-29 17:59:21 +0300

    Add files via upload

---
+++ src/main/java/com/example/lap5q3event/Controller/EventController.java
@@ -0,0 +1,68 @@
+package com.example.lap5q3event.Controller;
+import com.example.lap5q3event.Api.ApiResponse;
+import com.example.lap5q3event.Model.Event;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/Event")
+public class EventController {
+
+    ArrayList<Event> events = new ArrayList<Event>();
+
+@GetMapping("/get")
+    public ArrayList<Event> getEvents() {
+    return events;
+}
+@PostMapping("/add")
+    public ResponseEntity addEvent(@RequestBody Event event , Errors errors) {
+    if(errors.hasErrors()){
+        String message=errors.getFieldError().getDefaultMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+    events.add(event);
+    return ResponseEntity.status (200).body(new ApiResponse("Event Added Successfully"));
+}
+@PutMapping("/update/{index}")
+    public ResponseEntity updateEvent(@PathVariable int index, @RequestBody Event event ,Errors errors) {
+    if(errors.hasErrors()){
+        String message=errors.getFieldError().getDefaultMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+    events.set(index, event);
+    return ResponseEntity.status (200).body(new ApiResponse("Event Updated Successfully"));
+}
+@DeleteMapping("/delete/{index}")
+    public ResponseEntity deleteEvent(@PathVariable int index) {
+    events.remove(index);
+    return ResponseEntity.status(200).body(new ApiResponse("Event Deleted Successfully")) ;
+}
+
+@PutMapping("/capacity/{id}/{index}")
+    public ResponseEntity changeCapacity(@PathVariable int id ,@PathVariable int capacity ,@RequestBody Event event,Errors errors) {
+    if(errors.hasErrors()){
+        String message=errors.getFieldError().getDefaultMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+   for(Event e : events) {
+       if(e.getId() == id) {
+           e.setCapacity(capacity);
+       }
+   }
+    return ResponseEntity.status(200).body(new ApiResponse("Event Capacity Changed Successfully"));
+    }
+ @GetMapping("/get/search/{id}")
+ public ResponseEntity searchEventById(@PathVariable int id,@RequestBody Event event) {
+    for(Event e : events) {
+        if(e.getId() == id) {
+            return ResponseEntity.status(200).body(e);
+        }
+    }
+   return ResponseEntity.status(404).body(new ApiResponse("Event Not Found"));
+ }
+}
+
+
+
---
+++ src/main/java/com/example/lap5q3event/Model/Event.java
@@ -0,0 +1,37 @@
+package com.example.lap5q3event.Model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Data
+public class Event {
+
+@NotNull(message = "ID should be Not Null!")
+@Size(min = 3, max = 10 , message = "ID Size should be between 3 - 10")
+private int id;
+
+@NotNull(message = "Description should be Not Null!")
+@Size(min = 10 , max = 20)
+private String description;
+
+@NotNull(message = "ID should be Not Null!")
+@PositiveOrZero(message = "capacity should be a number")
+@Pattern(regexp = "(/^(?!(?:0|0\\.0|0\\.00)$)[+]?\\d+(\\.\\d|\\.\\d[0-9])?$/)",message = "")
+private int capacity;
+
+
+@JsonFormat
+@Pattern(regexp = "(yyyy-MM-dd)",message = "")
+private LocalDateTime startDate;
+
+
+@JsonFormat
+@Pattern(regexp = "(yyyy-MM-dd)",message = "")
+private LocalDateTime endDate;
+
+}
